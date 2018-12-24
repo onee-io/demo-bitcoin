ccbf46bacf083a9473838609f87fb9da192da347
onee <774247851@qq.com>
2018-12-24 22:13:45 +0800

    Initial commit

---
+++ src/core/block.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64		// 区块创建时间戳
+	Data          []byte	// 区块包含的数据
+	PrevBlockHash []byte	// 前一个区块的哈希值
+	Hash          []byte	// 区块自身的哈希值，用于校验区块数据有效
+}
+
+// 创建一个新区块
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+// 设置哈希值
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+}
